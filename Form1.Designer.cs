b7af78ca7e17765505d2778e2295cd2a42980178
Snow The Hacker <acommunicationerror@gmail.com>
2022-07-05 23:26:46 -0400

    initial commit
    Upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace StudioSDT
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1898, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(54, 29);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(58, 29);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(65, 29);
+            this.viewToolStripMenuItem.Text = "View";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(92, 29);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1898, 1024);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Closing += Window_Closing;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Windows;
+using System.Management;
+
+
+namespace StudioSDT
+{
+
+
+
+
+    public partial class Form1 : Form
+    {
+        public double thisZoom = 0.0;
+        public ChromiumWebBrowser chromeBrowser;
+
+        public System.Diagnostics.Process procs = new System.Diagnostics.Process();
+        public DateTime time = DateTime.Now;
+        public DateTime time2;
+
+        public static uint ColorToUInt(Color color)
+        {
+            return (uint)((color.A << 24) | (color.R << 16) | (color.G << 8) | (color.B << 0));
+        }
+
+       
+        public Form1()
+        {
+            string dir = Application.ExecutablePath.ToString();
+
+            dir = dir.Substring(0, dir.Length - 13);
+
+            string http = dir + "server\\http-server\\node_modules\\.bin\\http-server.cmd ";
+            string server = dir + "server";
+            procs.StartInfo.FileName = http;
+            procs.StartInfo.Arguments = server;
+            procs.Start();
+            
+            
+            
+
+            InitializeComponent();
+            CefSettings settings = new CefSettings();
+            //settings.CefCommandLineArgs.Add("disable-gpu");
+            settings.CefCommandLineArgs.Add("debug-plugin-loading");
+            settings.CefCommandLineArgs.Add("always-authorize-plugins");
+            settings.CefCommandLineArgs.Add("allow-outdated-plugins");
+            settings.CefCommandLineArgs.Add("no-sandbox-and-elevated");
+            //settings.CefCommandLineArgs.Add("ppapi-startup-dialog");
+
+            settings.CefCommandLineArgs.Add("allow-universal-access-from-files");
+            settings.CefCommandLineArgs.Add("dom-automation");
+            settings.CefCommandLineArgs.Add("ppapi-flash-args");
+
+            settings.CefCommandLineArgs.Add("allow-file-access-from-files");
+            //settings.CefCommandLineArgs.Add("ppapi-in-process");
+
+            settings.CefCommandLineArgs.Add("enable-pepper-testing");
+
+            settings.CefCommandLineArgs.Add("enable-gpu-plugin");
+            settings.CefCommandLineArgs.Add("enable-accelerated-plugins");
+
+            settings.CefCommandLineArgs.Add("user-data-dir");
+
+            //settings.CefCommandLineArgs.Add("enable-npapi", "1");
+            try
+            {
+               Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\WebTest" + "\\blob_storage", true);
+               Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\WebTest" + "\\Cache", true);
+               Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\WebTest" + "\\Code Cache", true);
+               Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\WebTest" + "\\GPUCache", true);
+               Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\WebTest" + "\\Local Storage", true);
+               Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\WebTest" + "\\Session Storage", true);
+               Directory.Delete(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\WebTest" + "\\Pepper Data", true);
+            }
+            catch { }
+
+            settings.CachePath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + @"\WebTest";
+
+            settings.CefCommandLineArgs.Add("allow-file-acccess-from-files");
+
+            if (settings.CefCommandLineArgs.ContainsKey("enable-system-flash"))
+                settings.CefCommandLineArgs.Remove("enable-system-flash");
+
+            settings.CefCommandLineArgs.Add("enable-system-flash", "1");
+            settings.CefCommandLineArgs.Add("disable-gpu-vsync", "0");
+            //settings.CefCommandLineArgs.Add("ppapi-flash-path", @"C:/libpepflashplayer.so");
+            //settings.CefCommandLineArgs.Add("ppapi-flash-version", "31.0.0.153.dll");
+
+            settings.CefCommandLineArgs["plugin-policy"] = "allow";
+
+            Cef.Initialize(settings, performDependencyCheck: false, browserProcessHandler: null);
+
+            InitializeChromium();
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+
+           time2 = DateTime.Now; 
+
+        }
+        public bool canclose = false;
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (!canclose)
+            {
+                e.Cancel = true;
+                canclose = true;
+            }
+            else 
+            {
+                return;
+            }
+
+           
+            var processes = Process.GetProcessesByName("node");
+
+            if (processes.Length == 0)
+            {
+                MessageBox.Show("Cannot find process Test");
+            }
+            else
+            {
+                foreach (var process in processes)
+                {
+                    if(process.StartTime >= time && process.StartTime <= time2)
+                        process.Kill();
+                }
+            }
+            
+            canclose = true;
+            this.Close();
+            
+
+
+        }
+
+
+
+
+        private void InitializeChromium()
+        {
+
+
+            var browserSettings = new BrowserSettings();
+
+            browserSettings.WebSecurity = CefState.Disabled;
+            browserSettings.FileAccessFromFileUrls = CefState.Enabled;
+            browserSettings.UniversalAccessFromFileUrls = CefState.Enabled;
+            browserSettings.BackgroundColor = ColorToUInt(System.Drawing.Color.FromArgb(255, 40, 42, 48));
+            browserSettings.Javascript = CefState.Enabled;
+
+            chromeBrowser = new ChromiumWebBrowser("http://127.0.0.1:8080/");
+
+            SplitContainer splitContainer = new SplitContainer();
+            this.Controls.Add(chromeBrowser);
+            //splitContainer.Dock = DockStyle.Fill;
+            //splitContainer.Panel2.Controls.Add(chromeBrowser);
+            //splitContainer.Orientation = Orientation.Horizontal;
+            //splitContainer.Size = new Size(1280, 720);
+            chromeBrowser.Dock = DockStyle.Fill;
+
+
+
+            chromeBrowser.BackColor = Color.FromArgb(255, 40, 42, 48);
+
+            chromeBrowser.BringToFront();
+
+            chromeBrowser.BrowserSettings = browserSettings;
+
+
+
+
+
+            chromeBrowser.IsBrowserInitializedChanged += (sender, args) =>
+            {
+                chromeBrowser.ShowDevTools();
+                chromeBrowser.SetZoomLevel(2.0);
+
+            };
+
+        }
+        
+        
+
+        public void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+        
+      
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+          
+        }
+    }
+}
